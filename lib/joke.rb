7bc11119f1f1365007e6f3783c55af623d89fd68
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-06 15:55:43 -0600

    initialize joke attributes

--- lib/joke.rb
+++ lib/joke.rb
@@ -1,3 +1,11 @@
 class Joke
 
+  attr_reader :id, :question, :answer
+
+  def initialize(joke_hash)
+    @id = joke_hash[:id]
+    @question = joke_hash[:question]
+    @answer = joke_hash[:answer]
+  end
+
 end
--- test/joke_test.rb
+++ test/joke_test.rb
@@ -5,4 +5,29 @@ require './lib/joke'
 
 class JokeTest < Minitest::Test
 
+  attr_reader :joke
+
+  def setup
+    @joke = Joke.new({id: 1,
+                      question: "Why did the strawberry cross the road?",
+                      answer: "Because his mother was in a jam."
+                    })
+  end
+
+  def test_it_exists
+    assert_instance_of Joke, joke
+  end
+
+  def test_can_get_joke_id
+    assert_equal 1, joke.id
+  end
+
+  def test_can_get_joke_question
+    assert_equal "Why did the strawberry cross the road?", joke.question
+  end
+
+  def test_can_get_joke_answer
+    assert_equal "Because his mother was in a jam.", joke.answer
+  end
+
 end
