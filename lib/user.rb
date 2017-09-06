8502f13fb93ba8f46b7050542925e18c59b572b0
Anna Lewis <anna.noel.lewis@gmail.com>
2017-09-06 16:33:59 -0600

    create learn_routine method; doesn't pass tests

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class User
 
   attr_reader :name
@@ -18,6 +20,24 @@ class User
 
   def perform_routine_for(user)
     user.jokes = @jokes
+  end
+
+  def learn_routine(file)
+    joke_array = []
+    CSV.foreach(file) do |line|
+      joke_array << line.split(', ')
+    end
+    joke_array.each do |joke|
+      id = joke[0]
+      question = joke[1]
+      answer = joke[2]
+      new_joke = Joke.new({
+                id: id,
+                question: question,
+                answer: answer
+              })
+      learn(new_joke)
+    end
   end
 
 end
